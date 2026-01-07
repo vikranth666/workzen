851a29b5422d2107b73a9bbf6d5784234326dd39
Vikranth bolleddula <160713044+vikranth666@users.noreply.github.com>
2026-01-07 12:41:03 +0530

    Update axios baseURL to include '/api'

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://workzen-x7tu.onrender.com"
+  baseURL: "https://workzen-x7tu.onrender.com/api"
 });
 
 instance.interceptors.request.use(config => {
