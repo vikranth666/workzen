aa6a591c7577730b70821ff158a7090e7769f98f
Vikranth bolleddula <160713044+vikranth666@users.noreply.github.com>
2026-01-07 11:55:15 +0530

    Change axios base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/api"
+  baseURL: "https://workzen-x7tu.onrender.com"
 });
 
 instance.interceptors.request.use(config => {
